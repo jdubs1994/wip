d7c4f1b2947f43cd50f2005add4af3b46dd650f8
James <duboisprogramming@gmail.com>
2025-07-05 22:12:14 -0400

    wip

--- rp_handler.py
+++ rp_handler.py
@@ -1,20 +1,30 @@
 import runpod
 import time  
+import os
+from huggingface_hub import login, snapshot_download
+import torch
 
 def handler(event):
-    print(f"Worker Start")
-    input = event['input']
-    
-    prompt = input.get('prompt')  
-    seconds = input.get('seconds', 0)  
+    HUGGING_FACE_TOKEN = os.getenv('HUGGING_FACE_TOKEN')
 
-    print(f"Received prompt: {prompt}")
-    print(f"Sleeping for {seconds} seconds...")
+    login(HUGGING_FACE_TOKEN)
     
-    # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
-    time.sleep(seconds)  
+    print(f"Worker Start")
+    # input = event['input']
     
-    return prompt 
+    
+    # prompt = input.get('prompt')  
+    # seconds = input.get('seconds', 0)  
+
+    # print(f"Received prompt: {prompt}")
+    # print(f"Sleeping for {seconds} seconds...")
+    
+    # # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
+    # time.sleep(seconds)  
+    
+    # return prompt 
+
+    return "Hello, World!"
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler })
